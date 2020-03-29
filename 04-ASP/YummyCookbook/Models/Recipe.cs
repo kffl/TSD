91f0b3deb049f48aec806368d2397d93c096a670
newt0npk <pawel@kuffel.io>
2020-03-29 18:33:24 +0200

    Let's start thinking 🟢: Add validation to at least thee attributes of recipe

--- 04-ASP/YummyCookbook/Models/Recipe.cs
+++ 04-ASP/YummyCookbook/Models/Recipe.cs
@@ -6,11 +6,14 @@ namespace YummyCookbook.Models
     public class Recipe
     {
         public int Id { get; set; }
+        [StringLength(120, MinimumLength = 5)]
         [Display(Name = "Recipe Name")]
         public string Name { get; set; }
-        [Display(Name = "Time to completion")]
+        [Range(5, 180)]
+        [Display(Name = "Time to completion (mins)")]
         public int Time { get; set; }
         public string Difficulty { get; set; }
+        [Range(1, int.MaxValue, ErrorMessage = "We don't h8 here, only positive values allowed.")]
         [Display(Name = "Number of likes")]
         public int NoLikes { get; set; }
         [Display(Name = "Ingredients")]
