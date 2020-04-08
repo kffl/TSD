182316b63fa9a7d8a8970684d1564184d0eff290
newt0npk <pawel@kuffel.io>
2020-04-08 23:08:46 +0200

    sufficient, 2/4 satisfactory

--- 06-WPF/WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ 06-WPF/WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,14 +16,56 @@ using System.Windows.Shapes;
 
 namespace WpfApp1
 {
+    public class MyClass
+    {
+
+    }
     /// <summary>
     /// Logika interakcji dla klasy MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        
+
+        private ObservableCollection<Book> BooksCollection = new ObservableCollection<Book>(MyBookCollection.GetMyCollection());
+
         public MainWindow()
         {
+            Loaded += MainWindow_Loaded;
             InitializeComponent();
+            MyListView.ItemsSource = BooksCollection;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            FormatComboBox.ItemsSource = Enum.GetValues(typeof(BookFormat)).Cast<BookFormat>();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            BooksCollection.Add(new Book());
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Do you want to delete selected book?",
+                                          "Confirmation",
+                                          MessageBoxButton.YesNo,
+                                          MessageBoxImage.Question);
+            if (result == MessageBoxResult.Yes)
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void Grid_Click(object sender, RoutedEventArgs e)
+        {
+            if (e.OriginalSource == DeleteButton)
+                BooksCollection.Remove((Book)MyListView.SelectedItem);
         }
     }
 }
